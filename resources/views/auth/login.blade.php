85e8821fc475d2ccd76047f9ed8447cad0ead824
BarokahAlways <a1234@1234s-MacBook-Pro.local>
2024-01-01 04:05:18 +0700

    Merapihkan Codingan

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,7 +28,7 @@
         @if (Route::has('password.request'))
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                 href="{{ route('password.request') }}">
-                {{ __('Forgot your password?') }}
+                {{ __('Forgot your password ?') }}
             </a>
         @endif
 
@@ -36,7 +36,7 @@
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                 href="{{ route('register') }}">
-                {{ __('Belum punya akun?') }}
+                {{ __('Belum punya akun ?') }}
             </a>
 
 
